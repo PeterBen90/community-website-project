9feac72a47d3e011a5154c507797408aabe7ef2a
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-23 23:30:40 -0400

    added nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import '../componentStyles/App.css';
 import ParticlesBackground from './Particles';
 import Header from './Header';
-import NavBar from './NavBar';
+// import NavBar from './NavBar';
+// import NavHeader from './NavHeader';
+import TopNav from './TopNav';
 
 class App extends Component {
   render() {
     return (
       <section className="App">
-        <NavBar />
+        <TopNav />
         <section className="landing-background">
           <ParticlesBackground />
           <Header />
