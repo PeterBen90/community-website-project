039cd3bcb5894ca12ebe575cd5c96150d46e71dd
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-07-13 14:14:26 -0400

    fixed iphone x view

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,85 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../componentStyles/TopNav.css';
-import Scroll from 'react-scroll';
-import Fade from 'react-reveal/Fade';
+import "../componentStyles/TopNav.css";
+import Scroll from "react-scroll";
+import Fade from "react-reveal/Fade";
 
 class TopNav extends Component {
-    render() {
-        let Link = Scroll.Link;
-        return (
-            <section className="Nav">
-                <Fade top>
-                    <div className="nav-name">
-                        <a
-                            onClick={() => (window.location = '/')}
-                            className="home-name"
-                        >
-                            <i class="fa fa-leaf" aria-hidden="true" />
-                        </a>
-                    </div>
-                </Fade>
-                <Fade top>
-                    <div className="header-nav">
-                        <nav className="nav-menu">
-                            <Link
-                                to="blog"
-                                offset={-40}
-                                spy={true}
-                                smooth={true}
-                                duration={700}
-                                className="nav-link"
-                            >
-                                Blog
-                            </Link>
-                            <Link
-                                to="guide"
-                                offset={10}
-                                spy={true}
-                                smooth={true}
-                                duration={700}
-                                className="nav-link"
-                            >
-                                Guide
-                            </Link>
-                            <Link
-                                to="Realty"
-                                offset={20}
-                                spy={true}
-                                smooth={true}
-                                duration={700}
-                                className="nav-link"
-                            >
-                                Realty
-                            </Link>
+  render() {
+    let Link = Scroll.Link;
+    return (
+      <section className="Nav">
+        <Fade top>
+          <div className="nav-name">
+            <a onClick={() => (window.location = "/")} className="home-name">
+              <i class="fa fa-leaf" aria-hidden="true" />
+            </a>
+          </div>
+        </Fade>
+        <Fade top>
+          <div className="header-nav">
+            <nav className="nav-menu">
+              <Link
+                to="blog"
+                offset={-40}
+                spy={true}
+                smooth={true}
+                duration={700}
+                className="nav-link"
+              >
+                Blog
+              </Link>
+              <Link
+                to="real-estate"
+                offset={10}
+                spy={true}
+                smooth={true}
+                duration={700}
+                className="nav-link"
+              >
+                Real Estate
+              </Link>
+              <Link
+                to="about"
+                offset={20}
+                spy={true}
+                smooth={true}
+                duration={700}
+                className="nav-link"
+              >
+                About
+              </Link>
 
-                            <Link
-                                to="about"
-                                offset={-80}
-                                spy={true}
-                                smooth={true}
-                                duration={700}
-                                className="nav-link"
-                            >
-                                About
-                            </Link>
-
-                            <Link
-                                to="contact"
-                                offset={-80}
-                                spy={true}
-                                smooth={true}
-                                duration={700}
-                                className="nav-link"
-                            >
-                                Contact
-                            </Link>
-                        </nav>
-                    </div>
-                </Fade>
-            </section>
-        );
-    }
+              <Link
+                to="contact"
+                offset={-80}
+                spy={true}
+                smooth={true}
+                duration={700}
+                className="nav-link"
+              >
+                Contact
+              </Link>
+            </nav>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
 }
 
 export default TopNav;
